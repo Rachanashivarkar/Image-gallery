755d256feeb4736e94fecaf727f3ee1bb0dc554f
Rachanashivarkar <rachanashivarkar4113@gmail.com>
2024-07-31 18:12:51 +0530

    iamge gallery array created

---
+++ script.js
@@ -0,0 +1,22 @@
+const images = [
+    {
+        src: './images/kyoto.jpg',
+        caption: ' Arashiyama Bamboo Grove, Kyoto'
+    },
+    {
+        src: './images/Miyazaki.jpg',
+        caption: 'Takachiho Gorge, Miyazaki'
+    },
+    {
+        src: './images/nara.jpg',
+        caption: 'Mount Yoshino, Nara'
+    },
+    {
+        src: './images/tokyo.jpg',
+        caption: 'Gardens of the Imperial Palace, Tokyo'
+    },
+    {
+        src: './images/toyama.jpg',
+        caption: 'Gokayama, Toyama'
+    }
+];
