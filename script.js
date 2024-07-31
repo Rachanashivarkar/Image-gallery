07ac30bd013896fd6e56c8db73851013e1003807
Rachanashivarkar <rachanashivarkar4113@gmail.com>
2024-07-31 18:50:03 +0530

    image gallery created

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-const images = [
-    {
-        src: './images/kyoto.jpg',
-        caption: ' Arashiyama Bamboo Grove, Kyoto'
-    },
-    {
-        src: './images/Miyazaki.jpg',
-        caption: 'Takachiho Gorge, Miyazaki'
-    },
-    {
-        src: './images/nara.jpg',
-        caption: 'Mount Yoshino, Nara'
-    },
-    {
-        src: './images/tokyo.jpg',
-        caption: 'Gardens of the Imperial Palace, Tokyo'
-    },
-    {
-        src: './images/toyama.jpg',
-        caption: 'Gokayama, Toyama'
+const imagesArr = [
+    "./images/kyoto.jpg",
+    "./images/Miyazaki.jpg",
+    "./images/nara.jpg",
+    "./images/tokyo.jpg",
+    "./images/toyama.jpg"
+  ];
+  
+  let imgIndex = 0;
+  const imgElement = document.getElementById('img');
+  
+  function prev() {
+    if (imgIndex === 0) {
+      imgIndex = imagesArr.length - 1;
+    } else {
+      imgIndex--;
     }
-];
+    imgElement.src = imagesArr[imgIndex];
+  }
+  
+  function next() {
+    if (imgIndex === imagesArr.length - 1) {
+      imgIndex = 0;
+    } else {
+      imgIndex++;
+    }
+    imgElement.src = imagesArr[imgIndex];
+  }
+  
